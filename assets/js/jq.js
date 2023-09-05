6efd5ae34e55780bf8a89043b140560408d5da92
Nuwan Rathnayake <x@nuwanr.com>
2023-09-05 18:24:05 +0530

    jQuery animations

---
+++ assets/js/jq.js
@@ -0,0 +1,13 @@
+// $.fx.off =true
+
+$('#btn').click(()=>{
+    // $('.box1').slideDown(400).delay(2000).fadeOut(1000)
+
+    $('.box2').animate({
+        width: '300px',
+        height: '10px',
+        color: 'red',
+    }, 2000)
+    
+
+})
